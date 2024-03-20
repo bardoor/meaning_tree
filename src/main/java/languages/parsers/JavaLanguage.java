f65e683b49c2a95339b90a915a0bd5818c5c7d7b
bardoor <dmitrybartenev58@gmail.com>
2024-03-20 23:39:34 +0300

    feat(JavaLanguage): Добавил класс для языка Java

--- src/main/java/languages/parsers/JavaLanguage.java
+++ src/main/java/languages/parsers/JavaLanguage.java
@@ -1,6 +1,24 @@
 package languages.parsers;
+import meaning_tree.*;
 import org.treesitter.*;
 
 public class JavaLanguage {
+    private final String _code = "";
+
+    public MeaningTree getMeaningTree(String code) {
+        TSParser parser = new TSParser();
+        TSLanguage javaLanguage = new TreeSitterJava();
+
+        parser.setLanguage(javaLanguage);
+        TSTree tree = parser.parseString(null, code);
+
+        return new MeaningTree(fromTSNode(tree.getRootNode()));
+    }
+
+    private Node fromTSNode(TSNode node) {
+        return null;
+    }
+
+
 
 }
