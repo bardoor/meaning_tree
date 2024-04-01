510b91e902db92dab22c8e4763f709eac9830285
bardoor <dmitrybartenev58@gmail.com>
2024-04-01 22:47:07 +0300

    feat(Node): Добавил генерацию уникального id через UUID

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/Node.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/Node.java
@@ -1,5 +1,11 @@
 package org.vstu.meaningtree.nodes;
 
+import java.util.UUID;
+
 abstract public class Node {
+    protected String _id = UUID.randomUUID().toString();
     public abstract String generateDot();
+    public String getId() {
+        return _id;
+    }
 }
