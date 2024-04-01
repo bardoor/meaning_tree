2195e596ee7ecf82d1972362f37aeadac814c814
brookit <brookit18@gmail.com>
2024-04-01 23:41:39 +0300

    chore(application): добавлен тестовый код на Python

--- modules/application/src/main/java/org/vstu/meaningtree/Main.java
+++ modules/application/src/main/java/org/vstu/meaningtree/Main.java
@@ -1,9 +1,9 @@
 package org.vstu.meaningtree;
-import org.vstu.meaningtree.languages.parsers.JavaLanguage;
+import org.vstu.meaningtree.languages.parsers.PythonLanguage;
 
 public class Main {
     public static void main(String[] args) {
-        JavaLanguage javaLanguage = new JavaLanguage();
-        MeaningTree mt = javaLanguage.getMeaningTree("if (5 + 10) {}");
+        PythonLanguage pyLanguage = new PythonLanguage();
+        MeaningTree mt = pyLanguage.getMeaningTree("if a:\n\ta * b\nelif b:\n\tt + 1\nelse:\n\tpass");
     }
 }
