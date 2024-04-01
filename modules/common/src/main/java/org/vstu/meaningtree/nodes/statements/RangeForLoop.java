49ed944c492ec8b30320c213c83914248afd6dd6
samedit66 <samedit66@yandex.ru>
2024-04-01 23:24:42 +0300

    docs(RangeForLoop): описал класс RangeForLoop, указал детали о диапазоне

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/RangeForLoop.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/RangeForLoop.java
@@ -2,12 +2,22 @@ package org.vstu.meaningtree.nodes.statements;
 
 import org.vstu.meaningtree.nodes.Statement;
 
+/**
+ * Цикл по диапазону целых чисел (начало и конец являются частью диапазна) с заданным шагом.
+ */
 public class RangeForLoop extends Statement {
     private final int _start;
     private final int _end;
     private final int _step;
     private final CompoundStatement _body;
 
+    /**
+     * Создает цикл по диапазону.
+     * @param start начало диапазона (включительно)
+     * @param end конец диапазона (включительно)
+     * @param step шаг
+     * @param body тело цикла
+     */
     public RangeForLoop(int start, int end, int step, CompoundStatement body) {
         _start = start;
         _end = end;
