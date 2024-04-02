30782b135735e2b80d296a3624f4cdc76eb0c56b
bardoor <dmitrybartenev58@gmail.com>
2024-04-02 03:41:38 +0300

    feat(Visualizer): Добавил вывод дерева на экран

--- modules/application/src/main/java/org/vstu/meaningtree/Main.java
+++ modules/application/src/main/java/org/vstu/meaningtree/Main.java
@@ -1,6 +1,7 @@
 package org.vstu.meaningtree;
 import org.vstu.meaningtree.languages.parsers.JavaLanguage;
 import org.vstu.meaningtree.languages.viewers.JavaViewer;
+import org.vstu.meaningtree.utils.Visualizer;
 
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ public class Main {
         System.setProperty("java.awt.headless", "false");
         JavaLanguage javaLanguage = new JavaLanguage();
         MeaningTree mt = javaLanguage.getMeaningTree("if (5 + 10) {12 * 55; if (12 - 6) {12; 13; 15;}} else {5 - 100;}");
-        mt.show();
+        Visualizer visualizer = new Visualizer(mt);
+        visualizer.visualize();
     }
 }
