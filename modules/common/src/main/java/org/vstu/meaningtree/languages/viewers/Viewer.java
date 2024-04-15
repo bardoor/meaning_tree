761c38359cdcebedfbacc96e5eff7aadb3563308
samedit66 <samedit66@yandex.ru>
2024-04-16 00:54:28 +0300

    feat: добавил метод, сразу переводящий MeaningTree в строку, без необходимости вызова getRootNode()

--- modules/common/src/main/java/org/vstu/meaningtree/languages/viewers/Viewer.java
+++ modules/common/src/main/java/org/vstu/meaningtree/languages/viewers/Viewer.java
@@ -1,7 +1,12 @@
 package org.vstu.meaningtree.languages.viewers;
 
+import org.vstu.meaningtree.MeaningTree;
 import org.vstu.meaningtree.nodes.Node;
 
 abstract public class Viewer {
     public abstract String toString(Node node);
+
+    public String toString(MeaningTree mt) {
+        return toString(mt.getRootNode());
+    }
 }
