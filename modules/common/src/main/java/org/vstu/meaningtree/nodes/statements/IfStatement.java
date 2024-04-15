5069776ff224cdae372ac17c162d95224243023d
bardoor <dmitrybartenev58@gmail.com>
2024-04-16 01:16:21 +0300

    feat(IfStatement): Добавил генерацию dot

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/IfStatement.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/IfStatement.java
@@ -39,16 +39,13 @@ public class IfStatement extends Statement {
 
     @Override
     public String generateDot() {
-        //TODO: fix for new structure
-        throw new UnsupportedOperationException();
-        /*
         StringBuilder builder = new StringBuilder();
         builder.append(String.format("%s [label=\"%s\"];\n", _id, getClass().getSimpleName()));
 
-        // builder.append(_body.generateDot());
-        // builder.append(_condition.generateDot());
-        // builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, _body.getId(), "body"));
-        // builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, _condition.getId(), "condition"));
+        for (ConditionBranch branch : _branches) {
+            builder.append(branch.generateDot());
+            builder.append(String.format("%s -- %s;\n", _id, branch.getId()));
+        }
 
         if (_elseBranch.isPresent()) {
             builder.append(_elseBranch.get().generateDot());
@@ -56,6 +53,5 @@ public class IfStatement extends Statement {
         }
 
         return builder.toString();
-        */
     }
 }
