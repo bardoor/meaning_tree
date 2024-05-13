fb356947cba02e057745ca3b1adc41927c4a8065
bardoor <dmitrybartenev58@gmail.com>
2024-05-14 00:35:46 +0300

    feat(PythonLanguage): Добавил генерацию dot и исправил наименование полей

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/WhileLoop.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/WhileLoop.java
@@ -4,24 +4,34 @@ import org.vstu.meaningtree.nodes.Expression;
 import org.vstu.meaningtree.nodes.Statement;
 
 public class WhileLoop extends Statement {
-    public Expression getCondition() {
-        return condition;
+    public Expression get_condition() {
+        return _condition;
     }
 
-    public Statement getBody() {
-        return body;
+    public Statement get_body() {
+        return _body;
     }
 
-    protected final Expression condition;
-    protected final Statement body;
+    protected final Expression _condition;
+    protected final Statement _body;
 
     public WhileLoop(Expression condition, Statement body) {
-        this.condition = condition;
-        this.body = body;
+        this._condition = condition;
+        this._body = body;
     }
 
     @Override
     public String generateDot() {
-        throw new UnsupportedOperationException();
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(String.format("%s [label=\"%s\"];\n", _id, getClass().getSimpleName()));
+
+        builder.append(_condition.generateDot());
+        builder.append(_body.generateDot());
+
+        builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, _condition.getId(), "condition"));
+        builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, _body.getId(), "body"));
+
+        return builder.toString();
     }
 }
