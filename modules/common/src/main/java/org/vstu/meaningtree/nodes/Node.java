50c102bac3009f8fe74c892838672e5e1ad03835
bardoor <dmitrybartenev58@gmail.com>
2024-05-13 22:10:40 +0300

    feat(Node): Добавил мегаобщий алгоритм генерации dot
    Работает покуда все поля класса - наследники Node

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/Node.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/Node.java
@@ -1,11 +1,33 @@
 package org.vstu.meaningtree.nodes;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicInteger;
 
 abstract public class Node {
     protected static AtomicInteger _id_generator = new AtomicInteger();
     protected Integer _id = _id_generator.incrementAndGet();
-    public abstract String generateDot();
+
+    public String generateDot(){
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(String.format("%s [label=\"%s\"];\n", _id, getClass().getSimpleName()));
+        Field[] fields = this.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Node node = null;
+            try {
+                if (field.get(this) instanceof Node) {
+                    node = (Node) field.get(this);
+                }
+            } catch (IllegalAccessException ignored) {}
+            if (node != null) {
+                builder.append(node.generateDot());
+                builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, node.getId(), field.getName()));
+            }
+        }
+
+        return builder.toString();
+    }
 
     public Integer getId() {
         return _id;
