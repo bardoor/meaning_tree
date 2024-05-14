81aef5fc301944f3d914f13abc547eecced20684
brookit <brookit18@gmail.com>
2024-05-14 12:14:12 +0300

    feat(Node): для каждого узла добавлена возможность получить детей в виде словаря

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/Node.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/Node.java
@@ -1,35 +1,40 @@
 package org.vstu.meaningtree.nodes;
 
 import java.lang.reflect.Field;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 abstract public class Node {
     protected static AtomicInteger _id_generator = new AtomicInteger();
     protected Integer _id = _id_generator.incrementAndGet();
 
-    public String generateDot(){
+    public String generateDot() {
         StringBuilder builder = new StringBuilder();
-
         builder.append(String.format("%s [label=\"%s\"];\n", _id, getClass().getSimpleName()));
-        Field[] fields = this.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            Node node = null;
-            try {
-                if (field.get(this) instanceof Node) {
-                    node = (Node) field.get(this);
-                }
-            } catch (IllegalAccessException ignored) {}
-            if (node != null) {
-                builder.append(node.generateDot());
-                builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, node.getId(), field.getName()));
-            }
+        Map<String, Node> nodes = getChildren();
+        for (String fieldName : nodes.keySet()) {
+            Node node = nodes.get(fieldName);
+            builder.append(node.generateDot());
+            builder.append(String.format("%s -- %s [label=\"%s\"];\n", _id, node.getId(), fieldName));
         }
-
         return builder.toString();
     }
 
     public Integer getId() {
         return _id;
+    }
+
+    public SortedMap<String, Node> getChildren() {
+        SortedMap<String, Node> map = new TreeMap<>();
+        Field[] fields = this.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                if (field.get(this) instanceof Node node) {
+                    map.put(field.getName(), node);
+                }
+            } catch (IllegalAccessException ignored) {}
+        }
+        return map;
     }
 }
