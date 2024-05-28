813b469e3bde868382e88521ab6f4dc8bf1a5c1e
samedit66 <samedit66@yandex.ru>
2024-05-28 16:45:14 +0300

    fix: добавил поддержку юникода во Viewer

--- modules/common/src/main/java/org/vstu/meaningtree/languages/parsers/Language.java
+++ modules/common/src/main/java/org/vstu/meaningtree/languages/parsers/Language.java
@@ -3,14 +3,17 @@ package org.vstu.meaningtree.languages.parsers;
 import org.vstu.meaningtree.MeaningTree;
 import org.treesitter.TSNode;
 
+import java.nio.charset.StandardCharsets;
+
 abstract public class Language {
     protected String _code = "";
 
     public abstract MeaningTree getMeaningTree(String code);
 
     protected String getCodePiece(TSNode node) {
+        byte[] code = _code.getBytes(StandardCharsets.UTF_8);
         int start = node.getStartByte();
         int end = node.getEndByte();
-        return _code.substring(start, end).trim();
+        return new String(code, start, end - start).trim();
     }
 }
