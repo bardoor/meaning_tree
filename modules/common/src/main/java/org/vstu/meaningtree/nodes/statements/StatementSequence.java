ec9fea5af847148c85c40a4d6d080c8f9606501e
brookit <brookit18@gmail.com>
2024-06-18 22:50:44 +0300

    feat(meaning_tree): Поддержка перечисления операторов в одном узле (пример: запятая в C++)

---
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/statements/StatementSequence.java
@@ -0,0 +1,19 @@
+package org.vstu.meaningtree.nodes.statements;
+
+import org.vstu.meaningtree.nodes.Statement;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StatementSequence extends Statement {
+    // Главным образом используется для оператора запятая в C++
+    private final List<Statement> _statements;
+
+    public StatementSequence(Statement ... stmts) {
+        _statements = Arrays.asList(stmts);
+    }
+
+    public List<Statement> getStatements() {
+        return _statements;
+    }
+}
