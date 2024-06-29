66e7825fbee1e1b37ca3b506ca7e842475d54df6
bardoor <dmitrybartenev58@gmail.com>
2024-06-30 01:40:05 +0300

    fix(TestCode): Пофиксил регулярки парсинга имени и кода

--- modules/test/src/main/java/org/vstu/meaningtree/TestCode.java
+++ modules/test/src/main/java/org/vstu/meaningtree/TestCode.java
@@ -2,33 +2,34 @@ package org.vstu.meaningtree;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class TestCode {
-    private final String _code;
-    private final String _language;
+    public final String code;
+    public final String language;
 
     public TestCode(String code) {
-        _language = parseName(code);
-        _code = parseCode(code);
+        language = parseName(code);
+        this.code = parseCode(code);
     }
 
     private String parseName(String testCode) {
-        Pattern langNamePattern = Pattern.compile("^\\s+(\\w+):\\s*$");
+        Pattern langNamePattern = Pattern.compile("^\\s+(\\w+):\\s*", Pattern.DOTALL | Pattern.MULTILINE);
         Matcher langNameMatcher = langNamePattern.matcher(testCode);
 
         if (!langNameMatcher.find()) {
             throw new IllegalArgumentException("Название языка в тест-кейсе не найдено!");
         }
-        return langNameMatcher.group();
+        return langNameMatcher.group(1);
     }
 
     private String parseCode(String testCode) {
         // Удалить пустые строки
         List<String> lines = Arrays.stream(testCode.split("\\R"))
-                                        .filter(String::isBlank)
+                                        .filter(Predicate.not(String::isBlank))
                                         .collect(Collectors.toList());
         // Удалить строку с названием языка (надеюсь что она первая)
         lines.removeFirst();
@@ -41,7 +42,7 @@ public class TestCode {
         return lines.stream().collect(Collectors.joining(System.lineSeparator()));
     }
 
-    public String getLanguage() { return _language; }
+    public String getLanguage() { return language; }
 
-    public String getCode() { return _code; }
+    public String getCode() { return code; }
 }
