a217e3ebbd5211f13289697a8966aebee2555df4
Dmitry Sh (Brookit) <brookit18@gmail.com>
2024-08-13 14:59:07 +0300

    feat(meaning_tree): узел вызова конструктора

---
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/ConstructorCall.java
@@ -0,0 +1,25 @@
+package org.vstu.meaningtree.nodes;
+
+import java.util.List;
+
+public class ConstructorCall extends Expression {
+    protected final List<Expression> _arguments;
+    protected final Type constructorOwner;
+
+    public ConstructorCall(Type constructorOwner, List<Expression> arguments) {
+        _arguments = arguments;
+        this.constructorOwner = constructorOwner;
+    }
+
+    public ConstructorCall(Type constructorOwner, Expression ... arguments) {
+        this(constructorOwner, List.of(arguments));
+    }
+
+    public List<Expression> getArguments() {
+        return _arguments;
+    }
+
+    public Type getOwner() {
+        return constructorOwner;
+    }
+}
