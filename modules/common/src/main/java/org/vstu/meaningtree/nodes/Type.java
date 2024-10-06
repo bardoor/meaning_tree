d1c184d00bb4a4f2e1e2453733a685d3a88c32c1
Dmitry Sh (Brookit) <brookit18@gmail.com>
2024-10-06 23:14:24 +0300

    feat(meaning_tree): Типы можно сделать константными

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/Type.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/Type.java
@@ -5,6 +5,17 @@ import org.vstu.meaningtree.nodes.expressions.Identifier;
 import java.util.Objects;
 
 public abstract class Type extends Identifier {
+    private boolean _isConst = false;
+
+    public boolean isConst() {
+        return _isConst;
+    }
+
+    // Чтобы не пришлось модифицировать конструкторы остальных типов
+    public void setConst(boolean state) {
+        _isConst = state;
+    }
+
     @Override
     public boolean equals(Object o) {
         return o.getClass().equals(this.getClass());
