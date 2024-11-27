373bd444ff8d644cd3770a363a968c01b46dea85
Dmitry Sh (Brookit) <brookit18@gmail.com>
2024-11-28 00:04:08 +0300

    chore(meaning_tree): Небольшие улучшения в классах выражений

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/expressions/literals/SetLiteral.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/expressions/literals/SetLiteral.java
@@ -2,8 +2,14 @@ package org.vstu.meaningtree.nodes.expressions.literals;
 
 import org.vstu.meaningtree.nodes.Expression;
 
+import java.util.List;
+
 public class SetLiteral extends PlainCollectionLiteral {
     public SetLiteral(Expression... content) {
+        super(content);
+    }
+
+    public SetLiteral(List<Expression> content) {
         super(content);
     }
 }
