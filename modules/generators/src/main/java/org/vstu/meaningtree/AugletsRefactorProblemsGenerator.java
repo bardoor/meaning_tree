288a8d0b215268d095d762815d510cb54be52a79
bardoor <dmitrybartenev58@gmail.com>
2024-12-16 04:39:27 +0300

    feat(generators): доабвил генератор для задачи дублированных блоков кода в case

--- modules/generators/src/main/java/org/vstu/meaningtree/AugletsRefactorProblemsGenerator.java
+++ modules/generators/src/main/java/org/vstu/meaningtree/AugletsRefactorProblemsGenerator.java
@@ -1,5 +1,7 @@
 package org.vstu.meaningtree;
 
+import org.apache.commons.lang3.IntegerRange;
+import org.apache.thrift.annotation.Nullable;
 import org.vstu.meaningtree.nodes.*;
 import org.vstu.meaningtree.nodes.ProgramEntryPoint;
 import org.vstu.meaningtree.nodes.expressions.BinaryExpression;
@@ -9,13 +11,16 @@ import org.vstu.meaningtree.nodes.expressions.logical.ShortCircuitAndOp;
 import org.vstu.meaningtree.nodes.statements.CompoundStatement;
 import org.vstu.meaningtree.nodes.statements.conditions.IfStatement;
 import org.vstu.meaningtree.nodes.statements.conditions.SwitchStatement;
-import org.vstu.meaningtree.nodes.statements.conditions.components.ConditionBranch;
+import org.vstu.meaningtree.nodes.statements.conditions.components.*;
 import org.vstu.meaningtree.nodes.statements.loops.DoWhileLoop;
 import org.vstu.meaningtree.nodes.statements.loops.WhileLoop;
 import org.vstu.meaningtree.utils.env.SymbolEnvironment;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class AugletsRefactorProblemsGenerator {
 
@@ -58,6 +63,7 @@ public class AugletsRefactorProblemsGenerator {
                 case WRAP_WHILE_LOOP_AND_REPLACE_IT_WITH_DO_WHILE ->
                         wrapWhileLoopAndReplaceItWithDoWhile((WhileLoop) node);
                 case CONVERT_REDUNDANT_CONDITION_CHECKS -> convertWithRedundantConditionChecks((IfStatement) node);
+                case ADD_DUPLICATED_CASE_BODIES -> addDuplicatedCaseBodies((SwitchStatement) node);
             };
         }
         catch (ClassCastException castException) {
@@ -114,8 +120,8 @@ public class AugletsRefactorProblemsGenerator {
      * </code></pre>
      *
      * @param ifStatement узел ветвления с ровно одной веткой <b>then</b> и веткой <b>else</b>
-     * @return {@code IfStatement} с модифицированной веткой <b>else</b>,
-     * <p>Или {@code null} если в переданном {@code IfStatement} отсутствует ветка <b>else</b> или несколько веток <b>then</b>
+     * @return {@link IfStatement} с модифицированной веткой <b>else</b>,
+     * <p>Или {@code null} если в переданном {@link IfStatement} отсутствует ветка <b>else</b> или несколько веток <b>then</b>
      */
     public static IfStatement addUselessConditionCheckingInElse(IfStatement ifStatement) {
         if (ifStatement.getBranches().size() != 1 || !ifStatement.hasElseBranch()) {
@@ -168,6 +174,40 @@ public class AugletsRefactorProblemsGenerator {
         );
     }
 
+    /**
+     * Преобразует множественное ветвление в серию одиночных <code>if</code> операторов с проверками условий
+     *
+     * <p>
+     *     Условия каждого блока <b>if</b> включают в себя логическое отрицание условий всех вышестоящих блоков <b>if</b>
+     * </p>
+     * <p>
+     *     Например:
+     *     <pre><code>
+     *     // До модификации
+     *     if (a > b) {
+     *         ...
+     *     } else if (a == b) {
+     *         ...
+     *     } else {
+     *         ...
+     *     }
+     *
+     *     // После модификации
+     *     if (a > b) {
+     *         ...
+     *     }
+     *     if (a <= b & a == b) {
+     *         ...
+     *     }
+     *     if (a <= b & a != b) {
+     *         ...
+     *     }
+     *     </code></pre>
+     * </p>
+     *
+     * @param ifStatement исходный узел с множественным ветвлением
+     * @return {@link CompoundStatement}, состоящий из серии одиночных {@link IfStatement}
+     */
     public static CompoundStatement convertWithRedundantConditionChecks(IfStatement ifStatement) {
         var andOpClass = ShortCircuitAndOp.class;
 
@@ -198,5 +238,91 @@ public class AugletsRefactorProblemsGenerator {
         return new CompoundStatement(new SymbolEnvironment(null), newIfStmts);
     }
 
+    /**
+     * Добавляет дублированные тела в <b>fallthrough case ветки</b>
+     *
+     * <p>
+     *     Если между несколькими блоками <b>case</b> присутствуют FallThrough ветки,
+     *     их пустые тела заменяются на тело первого далее найденного непустого case блока.
+     * </p>
+     * <p>
+     *     Пример:
+     *     <pre><code>
+     *     // До модификации
+     *     switch (x) {
+     *         case 1:
+     *         case 2:
+     *         case 3:
+     *             x += 1;
+     *             break;
+     *     }
+     *
+     *     // После модификации
+     *     switch (x) {
+     *         case 1:
+     *             x += 1;
+     *             break;
+     *         case 2:
+     *             x += 1;
+     *             break;
+     *         case 3:
+     *             x += 1;
+     *             break;
+     *     }
+     *     </code></pre>
+     * </p>
+     * <p>
+     *     Если <b>basicCase</b> не найден, то пустые блоки просто копируются без изменений.
+     * </p>
+     *
+     * @param switchStatement оператор <code>switch</code>
+     * @return {@link SwitchStatement} с дублированными блоками <b>case</b>
+     */
+    public static SwitchStatement addDuplicatedCaseBodies(SwitchStatement switchStatement) {
+        List<CaseBlock> cases = switchStatement.getCases();
+        List<CaseBlock> newCases = new ArrayList<>();
 
+        for (int i = 0; i < cases.size(); i++) {
+            int emptyCasesCount = 0;
+
+            while (cases.get(i) instanceof FallthroughCaseBlock) {
+                emptyCasesCount++;
+                i++;
+            }
+
+            if (emptyCasesCount == 0) {
+                newCases.add(cases.get(i));
+                continue;
+            }
+
+            var basicCase = getFirstBasicCaseCompoundStatement(cases, i);
+            newCases.addAll(
+                    basicCase == null
+                        ? cases.subList(i - emptyCasesCount, i)
+                        : cases.subList(i - emptyCasesCount, i).stream()
+                            .map(MatchValueCaseBlock.class::cast)
+                            .map(caseBlock -> new BasicCaseBlock(caseBlock.getMatchValue(), basicCase.getBody()))
+                            .toList()
+            );
+            newCases.add(basicCase);
+        }
+
+        return new SwitchStatement(switchStatement.getTargetExpression(), newCases, switchStatement.getDefaultCase());
+    }
+
+    /**
+     * Ищет первый непустой case
+     * @param cases список case
+     * @param from индекс начала поиска
+     * @return первый найденный непустой case или <code>null</code> если такового не нашлось
+     */
+    private static @Nullable BasicCaseBlock getFirstBasicCaseCompoundStatement(List<CaseBlock> cases, int from) {
+        return cases.subList(from, cases.size())
+                .stream()
+                .filter(BasicCaseBlock.class::isInstance)
+                .map(BasicCaseBlock.class::cast)
+                .filter(caseBlock -> caseBlock.getBody() != null)
+                .findFirst()
+                .orElse(null);
+    }
 }
