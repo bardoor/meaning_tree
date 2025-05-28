f196e856c3b78b7e22e0967d3690b1b25a4e01c1
Brookit <brookit18@gmail.com>
2025-05-28 20:25:03 +0300

    fix(values): Исправлены значения булевых литералов

--- modules/common/src/main/java/org/vstu/meaningtree/nodes/expressions/literals/BoolLiteral.java
+++ modules/common/src/main/java/org/vstu/meaningtree/nodes/expressions/literals/BoolLiteral.java
@@ -1,6 +1,8 @@
 package org.vstu.meaningtree.nodes.expressions.literals;
 
+import org.jetbrains.annotations.Nullable;
 import org.vstu.meaningtree.nodes.expressions.Literal;
+import org.vstu.meaningtree.utils.Label;
 
 import java.util.Objects;
 
@@ -9,7 +11,11 @@ public class BoolLiteral extends Literal {
 
     public BoolLiteral(boolean state) {
         _state = state;
+       this.setLabel(new Label(Label.VALUE, state));
     }
+
+    @Override
+    public void setAssignedValueTag(@Nullable Object obj) {}
 
     @Override
     public boolean equals(Object o) {
