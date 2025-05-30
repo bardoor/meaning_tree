8b371c40903f9c36d462e436d4574fa8b2475830
bardoor <dmitrybartenev58@gmail.com>
2025-05-31 02:06:18 +0300

    feat(languages): заменил использования конфига в Java

--- modules/languages/java/src/main/java/org/vstu/meaningtree/languages/JavaTranslator.java
+++ modules/languages/java/src/main/java/org/vstu/meaningtree/languages/JavaTranslator.java
@@ -1,6 +1,6 @@
 package org.vstu.meaningtree.languages;
 
-import org.vstu.meaningtree.languages.configs.ConfigParameter;
+import org.vstu.meaningtree.languages.configs.params.ExpressionMode;
 import org.vstu.meaningtree.utils.tokens.Token;
 import org.vstu.meaningtree.utils.tokens.TokenList;
 import org.vstu.meaningtree.utils.tokens.TokenType;
@@ -24,21 +24,27 @@ public class JavaTranslator extends LanguageTranslator {
 
     @Override
     public String prepareCode(String code) {
-        if (getConfigParameter("expressionMode").getBooleanValue() && !code.endsWith(";")) {
-            code = code + ";";
-        }
-        if (getConfigParameter("expressionMode").getBooleanValue()) {
+        boolean expressionMode = getConfigParameter(ExpressionMode.class).orElse(false);
+
+        if (expressionMode) {
+            if (!code.endsWith(";")) {
+                code += ";";
+            }
             code = String.format("class Main { public static void main(String[] args) {%s} }", code);
         }
+
         return code;
     }
 
     @Override
     public TokenList prepareCode(TokenList list) {
-        if (getConfigParameter("expressionMode").getBooleanValue() && !list.getLast().type.equals(TokenType.SEPARATOR)) {
-            list.add(new Token(";", TokenType.SEPARATOR));
-        }
-        if (getConfigParameter("expressionMode").getBooleanValue()) {
+        boolean expressionMode = getConfigParameter(ExpressionMode.class).orElse(false);
+
+        if (expressionMode) {
+            if (!list.getLast().type.equals(TokenType.SEPARATOR)) {
+                list.add(new Token(";", TokenType.SEPARATOR));
+            }
+
             TokenList final_ = getTokenizer().tokenize("class Main { public static void main(String[] args) {;%s} }", true);
             int marker = final_.indexOf(
                     final_.stream().filter((Token t) -> t.value.equals(";")).findFirst().orElse(null)
@@ -50,11 +56,7 @@ public class JavaTranslator extends LanguageTranslator {
             );
             return final_;
         }
-        return list;
-    }
 
-    @Override
-    protected ConfigParameter[] getDeclaredConfigParameters() {
-        return new ConfigParameter[0];
+        return list;
     }
 }
